84f0769a8ae6aad3cbb672d94b437d0da1b35c69
skarimo <karimovj@gmail.com>
2018-08-21 11:44:09 -0400

    fixed-update-or-delete

--- lib/cli/update_or_delete_a_card.rb
+++ lib/cli/update_or_delete_a_card.rb
@@ -33,5 +33,58 @@ end
 
 
 def update_card(user)
+  questions = user.questions
+  questions.each{|question| puts "ID: #{question.id}, Question:#{question.question}, Answer: #{question.answer}"}
+  puts "Please choose a question ID you want to update"
+  q_id = gets.chomp.to_i
+
+  question = Question.find(q_id)
+
+  puts "What would you like to update"
+  puts "(1) The Topic: #{question.topic}"
+  puts "(2) The Question: #{question.question}"
+  puts "(3) The Answer: #{question.answer}"
+    change = gets.chomp
+
+    if change == "1"
+      puts "What is the new topic?"
+      new_topic = gets.chomp
+      question.update(topic: new_topic)
+      puts "The topic has been updated"
+
+    elsif change == "2"
+      puts "What is the new question?"
+      new_question = gets.chomp
+      question.update(question: new_question)
+      puts "The question has been updated"
+
+    elsif change == "3"
+      puts "What is the new answer?"
+      new_answer = gets.chomp
+      question.update(answer: new_answer)
+      puts "The answer has been updated"
+    end
+
+end
+
+
+def delete_card(user)
+  questions = user.questions
+  questions.each{|question| puts "ID: #{question.id}, Question:#{question.question}, Answer: #{question.answer}"}
+  puts "Please choose a question to delete from your deck"
+  q_id = gets.chomp.to_i
+
+  question = Question.find(q_id)
+
+  deck = Deck.find_by(user_id:user.id, question_id:question.id)
+  Deck.destory(deck)
   
 end
+
+
+
+
+
+
+
+##
