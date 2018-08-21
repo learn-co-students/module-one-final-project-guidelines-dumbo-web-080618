c821cc8ae46edeff5bc73f1ce9e5b30d56b3373f
skarimo <karimovj@gmail.com>
2018-08-21 12:02:33 -0400

    update

--- lib/cli/update_or_delete_a_card.rb
+++ lib/cli/update_or_delete_a_card.rb
@@ -52,7 +52,7 @@ def update_card(user)
       question.update(topic: new_topic)
       puts "The topic has been updated"
       question.save
-      questions = user.questions
+      return
 
     elsif change == "2"
       puts "What is the new question?"
@@ -60,7 +60,7 @@ def update_card(user)
       question.update(question: new_question)
       puts "The question has been updated"
       question.save
-      questions = user.questions
+      return
 
     elsif change == "3"
       puts "What is the new answer?"
@@ -68,7 +68,7 @@ def update_card(user)
       question.update(answer: new_answer)
       puts "The answer has been updated"
       question.save
-      questions = user.questions
+      return
     end
 
 end
