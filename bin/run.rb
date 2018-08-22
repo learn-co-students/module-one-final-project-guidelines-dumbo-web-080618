0eeccf5458b34364c25c3250716873179d197667
vavnilov <vadim.avnilov@gmail.com>
2018-08-22 14:40:13 -0400

    more methods written

--- bin/run.rb
+++ bin/run.rb
@@ -7,32 +7,49 @@ $available_stock = Stock.all
 
 def self.greeting
   prompt = TTY::Prompt.new
-  puts "Hello, welcome to Sole Search!"
-  user_input = prompt.select("Would you like to do some Sole Searching today?",%w(Yes No))
+  puts "OH SHIT SON! Welcome to Sole Search... Looks like some NYC shops restocked some high heat pairs today so you're in luck..."
+  user_input = prompt.select("You tryna do some Sole Searching today or wut?",%w(Yes No))
   if user_input == "Yes"
-    puts "Great! Let's find you some fresh kicks!"
-    search
+    puts "That's what I thought my dude. Get ready to cop some serious fire."
+    location_search
   else
     exit
   end
 end
 
-def self.exit
-  puts "LATER LOSER!"
+def exit
+  puts "Enjoy those beaters on your feet! LATER LOSER!"
 end
 
-def self.search
+def location_search
   prompt = TTY::Prompt.new
-  user_input = prompt.multi_select("Where would you like to buy your shoes?",%w(Manhattan Brooklyn Bronx Staten\ Island Queens))
+  user_input = prompt.multi_select("Are you gonna go the distance to get the perfect pair? Pick as many locations as you want!",%w(Manhattan Brooklyn Bronx Staten\ Island Queens))
   $available_stores = Store.all.select {|store| user_input.include?(store.location)}
   choose_size
 end
 
-def self.choose_size
+def choose_size
   prompt = TTY::Prompt.new
-  user_input = prompt.multi_select("What size shoe would you like?",%w(5 6 7 8 9 10 11 12 13 14 15))
+  user_input = prompt.multi_select("What size you rock?",%w(7 8 9 10 11 12 13))
   $available_shoes = Shoe.all.select {|shoe| user_input.include?(shoe.size.to_s)}
-  puts $available_shoes
+  user_input = prompt.select("Does price really matter?",%w(Yea\ I'm\ ballin'\ on\ a\ budget Nah\ I'm\ just\ ballin'))
+  if user_input == "Yea I'm ballin' on a budget"
+    budget
+  else
+    finder
+  end
+end
+
+def budget
+  prompt = TTY::Prompt.new
+  user_input = prompt.select("How much cash you got?",%w(150 200 250 300))
+  user_input =user_input.to_i
+  $available_stock = $available_stock.select {|stock| stock.price <= user_input}
+  puts $available_stock
+  # finder
+end
+
+def finder
 end
 
 
