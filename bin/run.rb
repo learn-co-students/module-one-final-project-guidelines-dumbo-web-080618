20770a144c78f910862504987a8a40cb69e96b5b
vavnilov <vadim.avnilov@gmail.com>
2018-08-23 15:42:16 -0400

    Finishing touches added

--- bin/run.rb
+++ bin/run.rb
@@ -6,6 +6,12 @@ $available_stock = Stock.all
 
 def run
   prompt = TTY::Prompt.new(active_color: :cyan,help_color: :cyan)
+
+  puts "________     ______          ________                        ______"
+  puts "__  ___/________  /____      __  ___/__________ ________________  /_"
+  puts "_____ \\_  __ \\_  /_  _ \\     _____ \\_  _ \\  __ `/_  ___/  ___/_  __ \\"
+  puts "____/ // /_/ /  / /  __/     ____/ //  __/ /_/ /_  /   / /__ _  / / /"
+  puts "/____/ \\____//_/  \\___/      /____/ \\___/\\__,_/ /_/    \\___/ /_/ /_/"
   puts ""
   puts "OH SHIT SON! Welcome to Sole Search!"
   puts ""
