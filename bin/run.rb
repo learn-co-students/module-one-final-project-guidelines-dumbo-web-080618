ccee4c523803337d4161740f7d80c6b4f0efbe7e
mikepcarlos <michaelpcarlos@gmail.com>
2018-08-23 15:31:15 -0400

    done i guess

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 
+
 def current_time
   time = Time.new
 
@@ -30,10 +31,10 @@ def access_menu
   end
 end
 
-
 def main_menu
   prompt = TTY::Prompt.new
   current_weather = random_weather_condition
+
   prompt.select("
     _______       __   __                  ___ ___             __   __
 |   _   .-----|  |_|  |_.-----.----.   |   Y   .-----.---.-|  |_|  |--.-----.----.
@@ -49,7 +50,6 @@ def main_menu
     ") do |menu|
     menu.choice 'activity', current_weather
     menu.choice 'end', 'end'
-    clear_screen
   end
 end
 
@@ -58,54 +58,10 @@ def activity_menu(current_weather)
   random_activity = current_weather.weather_activities
   prompt.select("Today's weather is perfect for #{random_activity.sample}.") do |menu|
     menu.choice 'different suggestion', current_weather
-    #menu.choice 'choose activity', current_weather
     menu.choice 'end', 'end'
     clear_screen
   end
 end
 
-# access_menu
-binding.pry
-0
 
-#######################steps###############################
-
-
-
-#1st|b| - format intro - example below -- below intro is prompt for *next* or *exit* or *help*
-#_____________________________________________________________________________________
-#Good morning/afternoon/evening!. Can't think of something to do today?
-#Welcome to Better Weather CLI! Today's weather is #{weather instance}
-#        => next
-#        => help or 'h'
-#        => exit
-
-#   - find a way to fetch time off user's computer and use that information to determine
-#       whether to greet with morning/afternoon/night (current_time method)
-
-#   - define random weather method in weather class
-#
-#_____________________________________________________________________________________
-
-
-
-
-
-#2nd|a| - next - what happens when next is selected - outputs a random activity when
-#                   certain conditions are met
-
-#_________________________________________________________________
-#Today's weather is perfect for jogging!
-#             => Different activity!
-#             => exit
-#_________________________________________________________________
-
-
-
-
-
-#3rd|b| - exit - if exit is selected then output message
-
-#_________________________________________________________________
-#Thank you for visiting Better Weather CLI!
-#_________________________________________________________________
+access_menu
--- lib/activity.rb
+++ lib/activity.rb
@@ -3,7 +3,5 @@ class Activity < ActiveRecord::Base
   has_many :weather_activities
   has_many :weathers, through: :weather_activities
 
-  # def activities_for_weather
-  #   #list all activities for each weather condition
-  # end
+
 end
